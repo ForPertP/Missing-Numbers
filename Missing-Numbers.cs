7b0fc01c23081e7a3c14a6662dae1c21af813890
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-01-29 15:43:31 +0900

    Create missingNumbers function skeleton
    Added the skeleton for the missingNumbers function.

--- Missing-Numbers.cs
+++ Missing-Numbers.cs
@@ -11,3 +11,22 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<int> missingNumbers(List<int> arr, List<int> brr)
+    {
+
+    }
+
+}
